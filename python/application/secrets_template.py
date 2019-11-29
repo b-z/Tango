160b9e23fb92377bcddbb8ee48e9cbcce1b4c6e9
Zhōu Bówēi 周伯威 <github@bowei.me>
2019-11-29 19:27:58 +0900

    :shit: support to config secrets using enviroment variables

--- python/application/secrets_template.py
+++ python/application/secrets_template.py
@@ -1,5 +1,9 @@
-DB_IP = ''
-DB_PORT = ''
-DB_USER = ''
-DB_PASSWORD = ''
-DB_NAME = ''
+from os import environ as env
+
+DB_IP = env.get('TANGO_DB_IP') or ''
+DB_PORT = env.get('TANGO_DB_PORT') or ''
+DB_USER = env.get('TANGO_DB_USER') or ''
+DB_PASSWORD = env.get('TANGO_DB_PASSWORD') or ''
+DB_NAME = env.get('TANGO_DB_NAME') or ''
+
+# Note: You should put these into CircleCI Environment Variables.
